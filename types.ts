66f5ef6a82f9aca4f2b9021a49181416ea76da90
Farleir <farleir@gmail.com>
2025-07-19 01:25:00 -0300

    Cleaning

--- types.ts
+++ types.ts
@@ -1,21 +1,4 @@
-export type GameState = 'idle' | 'running' | 'paused' | 'finished' | 'bingo';
-
-export interface Cell {
-  number: number;
-  marked: boolean;
-  isFree?: boolean;
-}
-
-export interface BingoCardData {
-  id: number;
-  grid: Cell[][];
-}
-
-export enum Speed {
-  Slow = 3000,
-  Normal = 2000,
-  Fast = 1000,
-}
+export type GameState = 'idle' | 'running' | 'paused' | 'finished';
 
 export interface GameContextType {
   gameState: GameState;
